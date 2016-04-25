99e5bff442f1be242ff055f6e8997499a3a28116
richa1104 <richaa1104@gmail.com>
2016-04-25 14:43:48 +0530

    cache locations

--- src/Nuada/ApiBundle/Manager/LocationManager.php
+++ src/Nuada/ApiBundle/Manager/LocationManager.php
@@ -7,11 +7,13 @@ use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Validator\ValidatorInterface;
 use Symfony\Component\Finder\Finder;
 use Doctrine\DBAL\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class LocationManager
 {
     protected $doctrine;
     protected $securityContext;
+    protected $container;
 
     const LIMIT = 25;
     const OFFSET = 0;
@@ -19,12 +21,14 @@ class LocationManager
     public function __construct(Doctrine $doctrine,
                                 SecurityContextInterface $securityContext,
                                 ValidatorInterface $validator,
-                                Connection $legacyConnection)
+                                Connection $legacyConnection,
+                                ContainerInterface $container)
     {
         $this->doctrine = $doctrine;
         $this->securityContext = $securityContext;
         $this->validator = $validator;
         $this->legacyConnection = $legacyConnection;
+        $this->container = $container;
     }
 
     public function load($search=null) {
@@ -47,17 +51,32 @@ class LocationManager
 
     public function fetchAllLocations()
     {
-        $query = $this->legacyConnection->executeQuery("
-            SELECT DISTINCT comm
-            FROM (
-                 SELECT DISTINCT LTRIM(RTRIM(community)) as comm
-                        FROM bf_listing
-            ) A 
-            WHERE comm != ''
-            ORDER BY comm asc");
+        $fileName = 'locations.cache.php';
+        $dirName = 'cache';
+        $cache = $this->container->get('kernel')->getRootDir() .'/'.$dirName.'/'. $fileName;
+        
+        if (file_exists($cache)) {
+            $fh = fopen($cache, 'rb');
+            $locations = json_decode(fread($fh, filesize($cache)));
+            fclose($fh);
+        } else {
+            $query = $this->legacyConnection->executeQuery("
+                SELECT DISTINCT comm
+                FROM (
+                     SELECT DISTINCT LTRIM(RTRIM(community)) as comm
+                            FROM bf_listing
+                ) A 
+                WHERE comm != ''
+                ORDER BY comm asc");
 
-        $data = $query->fetchAll();
-        $locations = $this->hydrate($data);
+            $data = $query->fetchAll();
+            $locations = $this->hydrate($data);
+            
+            $fh = fopen($cache, 'w+') or die ('error writing location cache file');
+            fwrite($fh, json_encode($locations));
+            fclose($fh);
+
+        }
 
         return $locations;
     }
