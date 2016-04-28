eeac22e6a456ece5b75eb0c673a14decc616a50b
olympus-mons <prasoon.anand43@gmail.com>
2016-04-28 13:09:55 +0530

    Modified files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,9 @@ class AppKernel extends Kernel
             new Nuada\ApiBundle\NuadaApiBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            
+			new FOS\UserBundle\FOSUserBundle(),
+			new Nuada\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'prod'))) {
